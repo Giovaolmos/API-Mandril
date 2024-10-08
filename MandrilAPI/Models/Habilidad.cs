83bb55b307d6c05aab5ac9fc2923c0e49e543395
Giovaolmos <127804984+Giovaolmos@users.noreply.github.com>
2024-10-08 16:45:51 -0300

    first commit

---
+++ MandrilAPI/Models/Habilidad.cs
@@ -0,0 +1,15 @@
+namespace MandrilAPI.Models;
+
+public class Habilidad
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+
+    public enum EPotencia{
+        Suave,
+        Moderado,
+        Intenso,
+        RePotente,
+        Extremo
+    }
+}
---
+++ MandrilAPI/Models/Mandril.cs
@@ -0,0 +1,9 @@
+namespace MandrilAPI.models;
+public class Mandril 
+{
+    public int Id { get; set; }
+
+    public string Nombre  { get; set; } = string.Empty;
+
+    public string Apellido { get; set; } = string.Empty;
+}
