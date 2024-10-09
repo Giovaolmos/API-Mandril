b20c665dec23f20f9d7ce092ef37487b987b6aec
Giovaolmos <127804984+Giovaolmos@users.noreply.github.com>
2024-10-08 23:37:57 -0300

    post mandriles

--- MandrilAPI/Controllers/MandrilController.cs
+++ MandrilAPI/Controllers/MandrilController.cs
@@ -13,4 +13,33 @@ public class MandrilController : ControllerBase
     {
 return Ok(MandrilDataStore.Current.Mandriles);
     }
+
+    [HttpGet("{mandrilId}")]
+    public ActionResult<Mandril> GetMandrilById(int mandrilId)
+    {
+var mandril = MandrilDataStore.Current.Mandriles.FirstOrDefault(x => x.Id == mandrilId);
+
+if(mandril == null)
+return NotFound("El mandril solicitado no existe");
+return Ok(mandril);
+    }
+
+    [HttpPost]
+    public ActionResult<Mandril> PostMandril(MandrilInsert mandrilInsert)
+    {
+     var maxMandrilId = MandrilDataStore.Current.Mandriles.Max(x => x.Id);
+
+     var mandrilNuevo = new Mandril(){
+     Id = maxMandrilId + 1,
+     Nombre = mandrilInsert.Nombre,
+     Apellido = mandrilInsert.Apellido
+     };
+
+     MandrilDataStore.Current.Mandriles.Add(mandrilNuevo);
+
+     return CreatedAtAction(nameof(GetMandrilById), 
+     new { mandrilId = mandrilNuevo.Id },
+     mandrilNuevo
+     );
+    }
 }
---
+++ MandrilAPI/Models/MandrilInster.cs
@@ -0,0 +1,7 @@
+namespace MandrilAPI;
+public class MandrilInsert
+{
+    public string Nombre  { get; set; } = string.Empty;
+
+    public string Apellido { get; set; } = string.Empty;
+}
